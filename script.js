5a392054b5ee5bb255b967622c62016deeb0d256
Thibaud Joel Hadamczik <thadamczik@gmail.com>
2024-09-25 20:57:24 +0200

    Add function to get human input

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     return randomNumber === 0 && "rock" || randomNumber === 1 && "paper" || randomNumber === 2 && "scissors";
 }
+
+function getHumanChoice() {
+    let humanInput = prompt("Enter 'Rock', 'Paper' or 'Scissors':").toLowerCase();
+    while (humanInput !== "rock" && humanInput !== "paper" && humanInput !== "scissors") {
+        humanInput = prompt("This was not a valid choice. Please enter 'Rock', 'Paper' or 'Scissors':")
+    }
+    return humanInput;
+}
